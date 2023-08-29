ecd565604d169998223da3800bb1f0f63a01399a
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-29 05:54:03 -0400

    Update user_data.sh

--- task1/terraform/user_data.sh
+++ task1/terraform/user_data.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-
-# Update package lists for upgrades and new package installations
-apt-get update
-
-# Upgrade the instance with the latest patches
-apt-get upgrade -y
-
-# Ensure the SSH server is installed (it typically is by default)
-apt-get install -y openssh-server
-
-# Ensure that SSH is set to start on boot
+apt update && apt upgrade -y
+apt install -y openssh-server
 systemctl enable ssh
-
-# Start the SSH service just in case it's not running
 systemctl start ssh
-
-# Optional: Install and set up any additional software or tools if required. For example:
-# apt-get install -y apache2
-# systemctl enable apache2
-# systemctl start apache2
