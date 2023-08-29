03f725a67e45e2f4eaec90e2eb7d8a668dab87dc
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-29 13:44:26 -0400

    Update user_data.sh

--- task1/terraform/user_data.sh
+++ task1/terraform/user_data.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+# Update the system
+apt-get update
+apt-get install -y openjdk-11-jdk ansible
+
+# Ensure the .ssh directory exists
+mkdir -p /home/ubuntu/.ssh
+chmod 700 /home/ubuntu/.ssh
+
+# Generate an RSA key pair only if it doesn't already exist
+if [ ! -f /home/ubuntu/.ssh/id_rsa ]; then
+    ssh-keygen -t rsa -f /home/ubuntu/.ssh/id_rsa -N ""
+fi
+
+# Set proper permissions and ownership
+chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+chmod 600 /home/ubuntu/.ssh/id_rsa
+chmod 644 /home/ubuntu/.ssh/id_rsa.pub
+
+# Embed the Jenkins public key
+echo "sec_com_ass_key_pair" >> /home/ubuntu/.ssh/authorized_keys
+
+# Set proper permissions for the authorized_keys file
+chmod 600 /home/ubuntu/.ssh/authorized_keys
+
+# Rest of your script logic, if any...
+
 apt update && apt upgrade -y
 apt install -y openssh-server
 systemctl enable ssh
