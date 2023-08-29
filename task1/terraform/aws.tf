a83f9d2bc82608966c9619c4c0a6e49de5725f3a
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-29 05:38:13 -0400

    Update aws.tf

--- task1/terraform/aws.tf
+++ task1/terraform/aws.tf
@@ -5,7 +5,6 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "Main VPC"
   }
@@ -15,7 +14,6 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "main_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
-
   tags = {
     Name = "Main Subnet"
   }
@@ -25,16 +23,17 @@ resource "aws_subnet" "main_subnet" {
 resource "aws_security_group" "ec2_sg" {
   vpc_id      = aws_vpc.main_vpc.id
   name        = "ec2_sg"
-  description = "Allow SSH from Jenkins and HTTP"
+  description = "Allow SSH and HTTP"
 
-  # Open SSH port only for Jenkins IP
+  # Allow SSH only from Jenkins IP (replace <YOUR_JENKINS_IP> with your Jenkins server IP)
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["54.244.39.147/32"]  # Replace with your Jenkins Server IP
+    cidr_blocks = ["54.244.39.147/32"]
   }
 
+  # HTTP remains open to the world; consider limiting this in a production environment
   ingress {
     from_port   = 80
     to_port     = 80
@@ -42,6 +41,7 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow all outgoing traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -56,17 +56,12 @@ resource "aws_security_group" "ec2_sg" {
 
 # Create the EC2 instance within the VPC and subnet
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0261755bbcb8c4a84"
+  ami           = "ami-0261755bbcb8c4a84" # Make sure this AMI is available in your region
   instance_type = "t2.large"
   subnet_id              = aws_subnet.main_subnet.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
-  key_name                    = "sec_com_ass_key_pair"
-
-  # Introduce a delay to ensure SSH is ready on EC2 (optional, based on your needs)
-  provisioner "local-exec" {
-    command = "sleep 120"
-  }
+  key_name                    = "sec_com_ass_key_pair" # Ensure this key pair exists in AWS and the private key is on Jenkins
 
   tags = {
     Name = "EC2_INSTANCE_BY_JENKINS"
