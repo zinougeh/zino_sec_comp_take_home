c5a5d59cfe6d293b17abb310d231b3e85b473c9b
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-29 18:58:31 -0400

    Update user_data.sh

--- task1/terraform/user_data.sh
+++ task1/terraform/user_data.sh
@@ -4,37 +4,37 @@ apt-get update
 apt-get install -y openjdk-17-jdk ansible
 
 # Ensure the .ssh directory exists
-mkdir -p /home/ubuntu/.ssh
-chmod 700 /home/ubuntu/.ssh
+#mkdir -p /home/ubuntu/.ssh
+#chmod 700 /home/ubuntu/.ssh
 
 # Generate an RSA key pair only if it doesn't already exist
-if [ ! -f /home/ubuntu/.ssh/id_rsa ]; then
-    ssh-keygen -t rsa -f /home/ubuntu/.ssh/id_rsa -N ""
-fi
+#if [ ! -f /home/ubuntu/.ssh/id_rsa ]; then
+#    ssh-keygen -t rsa -f /home/ubuntu/.ssh/id_rsa -N ""
+#fi
 
 # Set proper permissions and ownership
-chown -R ubuntu:ubuntu /home/ubuntu/.ssh
-chmod 600 /home/ubuntu/.ssh/id_rsa
-chmod 644 /home/ubuntu/.ssh/id_rsa.pub
+#chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+#chmod 600 /home/ubuntu/.ssh/id_rsa
+#chmod 644 /home/ubuntu/.ssh/id_rsa.pub
 
 # Embed the Jenkins public key
-echo "sec_com_ass_key_pair" >> /home/ubuntu/.ssh/authorized_keys
+#echo "sec_com_ass_key_pair" >> /home/ubuntu/.ssh/authorized_keys
 
 # Set proper permissions for the authorized_keys file
-chmod 600 /home/ubuntu/.ssh/authorized_keys
+#chmod 600 /home/ubuntu/.ssh/authorized_keys
 
 # Specify the desired directory
-TARGET_DIR="/var/lib/jenkins/workspace/sec_comp_pipline/zino_sec_comp_take_home/task1/terraform"
+#TARGET_DIR="/var/lib/jenkins/workspace/sec_comp_pipline/zino_sec_comp_take_home/task1/terraform"
 
 # Check if the directory exists, if not create it
-if [ ! -d "$TARGET_DIR" ]; then
-    mkdir -p "$TARGET_DIR"
-fi
+#if [ ! -d "$TARGET_DIR" ]; then
+#    mkdir -p "$TARGET_DIR"
+#fi
 
 # Copy the id_rsa.pub file to the target directory
 # Ensure that the source path is correct for your setup.
 # In this example, it's assumed that the public key is present in the default .ssh directory of the user.
-cp ~/.ssh/id_rsa.pub "$TARGET_DIR/"
+#cp ~/.ssh/id_rsa.pub "$TARGET_DIR/"
 
 # Rest of your script logic, if any...
 
