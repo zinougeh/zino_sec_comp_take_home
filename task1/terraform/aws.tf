1e528f89ca8e66f3e378d7452b5d944d3a9a95eb
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-29 13:55:55 -0400

    Update aws.tf

--- task1/terraform/aws.tf
+++ task1/terraform/aws.tf
@@ -1,8 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
-# Create a VPC
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -10,65 +9,90 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-# Create a Subnet within the VPC
 resource "aws_subnet" "main_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
   tags = {
     Name = "Main Subnet"
   }
 }
 
-# Create a Security Group within the VPC
-resource "aws_security_group" "ec2_sg" {
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
   vpc_id      = aws_vpc.main_vpc.id
-  name        = "ec2_sg"
-  description = "Allow SSH and HTTP"
 
-  # Allow SSH only from Jenkins IP (replace <YOUR_JENKINS_IP> with your Jenkins server IP)
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["54.244.39.147/32"]
-  }
-
-  # HTTP remains open to the world; consider limiting this in a production environment
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outgoing traffic
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "EC2_SG_BY_JENKINS"
-  }
 }
 
-# Create the EC2 instance within the VPC and subnet
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0261755bbcb8c4a84" # Make sure this AMI is available in your region
-  instance_type = "t2.large"
+  ami           = "ami-09d95fab7fff3776c"  # Ubuntu Server 20.04 LTS
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id              = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
-  key_name                    = "sec_com_ass_key_pair" # Ensure this key pair exists in AWS and the private key is on Jenkins
+  
+  user_data = file("user_data.sh")
 
   tags = {
-    Name = "EC2_INSTANCE_BY_JENKINS"
+    Name = "MicroK8s-Instance"
   }
 }
 
-# Output the public IP of the instance
-output "public_ip" {
-  value = aws_instance.ec2_instance.public_ip
+resource "aws_key_pair" "deployer" {
+  key_name   = "sec_com_ass_key_pair"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_eip" "eip_alloc" {
+  instance = aws_instance.ec2_instance.id
+  tags = {
+    Name = "EC2_INSTANCE_EIP"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_vpc.main_vpc.main_route_table_id
+  }
+  tags = {
+    Name = "Main Route Table"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2_instance.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_eip.eip_alloc.public_ip
+}
+
+output "instance_private_ip" {
+  description = "Private IP of the EC2 instance"
+  value       = aws_instance.ec2_instance.private_ip
 }
