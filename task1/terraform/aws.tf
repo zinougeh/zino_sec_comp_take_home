d2dbbb51cd7367edfbfb5c5991d725d600cacf6b
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-30 11:43:50 -0400

    Update aws.tf

--- task1/terraform/aws.tf
+++ task1/terraform/aws.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "allow_ssh_and_http" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["54.244.39.147/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
   # HTTP ingress
