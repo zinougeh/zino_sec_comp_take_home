fa25c4aa0e9216bd508ce4285986a63dbed1a5b3
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-30 05:32:39 -0400

    Update user_data.sh

--- task1/terraform/user_data.sh
+++ task1/terraform/user_data.sh
@@ -7,5 +7,3 @@ apt-get install -y openjdk-17-jdk ansible openssh-server
 # Start and enable SSH
 systemctl start ssh
 systemctl enable ssh
-
-# ... [Rest of the logic, if you want to keep commented-out lines, else you can remove them]
