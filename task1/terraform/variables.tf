6406f0776d8b96555f5d3a70f970ea6dc0a9a800
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-31 15:50:14 -0400

    Update variables.tf

--- task1/terraform/variables.tf
+++ task1/terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "public_key_path" {
   description = "Path to the public key file."
   type        = string
-  default     = "/home/ubuntu/.ssh/id_rsa.pub"
+  default     = "/home/jenkins/.ssh/id_rsa.pub"
 }
