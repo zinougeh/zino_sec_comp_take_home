1b3ca44622926c06271efe5a8a21a9e1fc4b36cd
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-08-30 21:03:04 -0400

    Update user_data.sh

--- task1/terraform/user_data.sh
+++ task1/terraform/user_data.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Update and upgrade system packages
+#Update and upgrade system packages
 sudo apt-get update -y && apt-get upgrade -y
 
-# Install and enable SSH
+#Install and enable SSH
 sudo apt-get install openssh-server -y
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
-# Install microk8s
+#Install microk8s
 sudo snap install microk8s --classic
 sudo microk8s.start
 
-# Add the Ubuntu user to the 'microk8s' group
+#Add the Ubuntu user to the 'microk8s' group
 sudo usermod -a -G microk8s ubuntu
 
 # Enable microk8s addons
 sudo microk8s.enable dashboard dns registry istio
-
 # Configure kubectl for the user
 sudo microk8s.kubectl config view --raw > $HOME/.kube/config
 
