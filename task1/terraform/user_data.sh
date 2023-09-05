93997752a29d94707941b2298d028b348f5db96d
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-09-05 19:20:50 -0400

    Update user_data.sh

--- task1/terraform/user_data.sh
+++ task1/terraform/user_data.sh
@@ -8,14 +8,41 @@ sudo apt-get install openssh-server -y
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
+#Install IP Tables
+sudo apt update
+sudo apt install iptables
+
+#To make rules persist after a reboot
+sudo apt install iptables-persistent
+
+
+#This command will forward all incoming traffic on port 80 to port 30080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 30080
+
+#To save the current iptables rules
+sudo sh -c "iptables-save > /etc/iptables/rules.v4"
+
+
+# create user for installations
+sudo useradd /home/jenkins
+
 # Append the SSH public key to the authorized_keys of the Ubuntu user
 sudo echo "${ssh_public_key}" >> /home/ubuntu/.ssh/authorized_keys
+
+# Append the SSH public key to the authorized_keys of the Ubuntu user
+sudo echo "${ssh_public_key}" >> /home/jenkins/.ssh/authorized_keys
 
 # Set correct permissions
 sudo chmod 700 /home/ubuntu/.ssh
 
 # Set correct permissions for the authorized_keys file
 sudo chmod 600 /home/ubuntu/.ssh/authorized_keys
+
+# Set correct permissions
+sudo chmod 700 /home/jenkins/.ssh
+
+# Set correct permissions for the authorized_keys file
+sudo chmod 600 /home/jenkins/.ssh/authorized_keys
 
 #Install microk8s
 sudo snap install microk8s --classic
