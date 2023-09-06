bb434d6a4238f1d8bc49494fd0244a5562412bc7
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-09-06 04:58:57 -0400

    Update aws.tf

--- task1/terraform/aws.tf
+++ task1/terraform/aws.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "us-west-1"
 }
 
+variable "public_key_path" {
+  description = "Path to the SSH Public Key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
