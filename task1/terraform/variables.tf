c3f9f4ffd9360591279df94608fd907e03b265c9
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-09-06 05:08:30 -0400

    Update variables.tf

--- task1/terraform/variables.tf
+++ task1/terraform/variables.tf
@@ -3,3 +3,13 @@
 #  type        = string
 #  default     = "/var/lib/jenkins/.ssh/id_rsa.pub"
 #}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = var.ssh_public_key
+}
+
+
+
+
+
