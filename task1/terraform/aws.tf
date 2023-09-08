c51ae91a781b1e88913bc501e97f7e16aef48edf
Zino Ugeh <58949345+zinougeh@users.noreply.github.com>
2023-09-07 20:32:08 -0400

    Update aws.tf

--- task1/terraform/aws.tf
+++ task1/terraform/aws.tf
@@ -63,28 +63,28 @@ resource "aws_eip" "eip_alloc" {
 }
 
 //resource "aws_internet_gateway" "main_gw" {
-  vpc_id = "vpc-0b2b089d46f9cbb7d" # Direct VPC ID used
-  tags = {
-    Name = "Main Internet Gateway"
-  }
-}
+ // vpc_id = "vpc-0b2b089d46f9cbb7d" # Direct VPC ID used
+//  tags = {
+//    Name = "Main Internet Gateway"
+//  }
+//}
 
-resource "aws_route_table" "route_table" {
-  vpc_id = "vpc-0b2b089d46f9cbb7d" # Direct VPC ID used
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main_gw.id
-  }
+//resource "aws_route_table" "route_table" {
+//  vpc_id = "vpc-0b2b089d46f9cbb7d" # Direct VPC ID used
+//  route {
+//    cidr_block = "0.0.0.0/0"
+//    gateway_id = aws_internet_gateway.main_gw.id
+//  }
 
-  tags = {
-    Name = "Main Route Table"
-  }
-}
+//  tags = {
+//    Name = "Main Route Table"
+//  }
+//}
 
-resource "aws_route_table_association" "route_table_association" {
-  subnet_id      = aws_subnet.main_subnet.id
-  route_table_id = aws_route_table.route_table.id
-}//
+//resource "aws_route_table_association" "route_table_association" {
+//  subnet_id      = aws_subnet.main_subnet.id
+//  route_table_id = aws_route_table.route_table.id
+//}
 
 output "instance_id" {
   description = "ID of the EC2 instance"
